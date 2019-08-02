cd757d96a499f5ddacf82492c1b07a61bcd29ccd
ryanalco <ryanac11@gmail.com>
2019-08-01 17:37:44 -0700

    added some additional datasets for testing and started looking at NTLK processing of raw text

--- Flair-CoNLL.py
+++ Flair-CoNLL.py
@@ -5,19 +5,35 @@ Created on Tues Jul 23 12:29:48 2019
 """
 
 from flair.data import Corpus
-from flair.datasets import ColumnCorpus
+#from flair.datasets import ColumnCorpus
+import flair.datasets
 from flair.embeddings import TokenEmbeddings, WordEmbeddings, StackedEmbeddings, PooledFlairEmbeddings
 from typing import List
-#from flair.data import Sentence
+import nltk, re, pprint
+from nltk import word_tokenize
 
+'''
+#Perform NER on CoNLL dataset:
 columns = {0: 'text', 1: 'pos', 2: 'synt', 3: 'ner'}
 
 data_folder = 'resources'
 
-corpus: Corpus = ColumnCorpus(data_folder, columns,
+corpus: Corpus = flair.datasets.ColumnCorpus(data_folder, columns,
                               train_file='eng.train.txt',
                               test_file='eng.testa.txt').downsample(.1)
                               #dev_file='eng.testb.txt')
+'''                              
+ 
+#Perform NER on a different already prepared dataset:                             
+#corpus = flair.datasets.WIKINER_ENGLISH().downsample(0.1)  
+                              
+                              
+#Perform flair on Biology dataset:
+columns = {0: 'text', 1: 'ner'}
+data_folder = 'resources'
+corpus: Corpus = flair.datasets.ColumnCorpus(data_folder, columns,
+                              train_file='BiologyNer.txt').downsample(.1)                      
+                              
 
 #corpus = corpus.downsample(.4)
 
@@ -49,12 +65,14 @@ tagger: SequenceTagger = SequenceTagger(hidden_size = 256,
                                         embeddings = embeddings,
                                         tag_dictionary = tag_dictionary,
                                         tag_type = tag_type)
+                                        #use_crf=True)
 
 from flair.trainers import ModelTrainer
 
 trainer: ModelTrainer = ModelTrainer(tagger, corpus)
 
-trainer.train('eng.testb.txt',
+trainer.train('eng.train.txt',
+            #'resources/taggers/example-ner',
               learning_rate=0.1,
               mini_batch_size=32,
               max_epochs=10)
@@ -78,15 +96,17 @@ for token in sentence:
 
 
 
-
-
+#Beginning NLTK processing on raw biology test
 '''
-try:
-    with open('/documents/resources', 'r') as fh:
-        print ('found')
-        # Store configuration file values
-except FileNotFoundError:
-    print ('not found')
-    # Keep preset values
+raw = open('BioTest.txt').read()
+tokens = word_tokenize(raw)
+
+def ie_preprocess(document):
+    sentences = nltk.sent_tokenize(document) 
+    sentences = [nltk.word_tokenize(sent) for sent in sentences]
+    sentences = [nltk.pos_tag(sent) for sent in sentences]
+
+'''    
     
-'''
+    
+    
